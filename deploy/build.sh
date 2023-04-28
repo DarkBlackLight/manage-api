d8fa6b463a6a4f1d787cd62c4ed2f0007212fd52
Wuqi Li <liwuqi2006@gmail.com>
2023-04-28 10:13:00 -0400

    minor fix

--- deploy/build.sh
+++ deploy/build.sh
@@ -7,5 +7,5 @@ sudo rm ${APP_NAME}_app.tar.gz
 
 sudo docker-compose build
 
-sudo docker save ${APP_NAME}_web | gzip > ${APP_NAME}_web.tar.gz
-sudo docker save ${APP_NAME}_app | gzip > ${APP_NAME}_app.tar.gz
+#sudo docker save ${APP_NAME}_web | gzip > ${APP_NAME}_web.tar.gz
+#sudo docker save ${APP_NAME}_app | gzip > ${APP_NAME}_app.tar.gz
