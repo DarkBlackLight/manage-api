e33a39957ae53e67592db034421d9809c7554a60
Wuqi Li <liwuqi2006@gmail.com>
2023-09-28 12:24:38 -0400

    minor fix

--- app/controllers/concerns/manage_storage_controller_concern.rb
+++ app/controllers/concerns/manage_storage_controller_concern.rb
@@ -7,7 +7,7 @@ module ManageStorageControllerConcern
         @resource.storage.attach(params[:storage])
 
         if @resource.save
-          render json: { data: @resource.as_json(only:[:id], methods: :src) }
+          render json: { data: @resource.as_json(only: [:id], methods: [:src, :filename]) }
         else
           render json: { data: 'Something went wrong.' }, status: :unprocessable_entity
         end
